3aa70c600ca6a9db3bfb2a074e0c6b2c68e4ccee
azizturki99 <aziz.turky.99@gmail.com>
2024-01-26 13:21:01 +0100

    done

--- pair.js
+++ pair.js
@@ -1,5 +1,58 @@
-var loadFile = function (event) {
-    var image = document.getElementById("output");
-    image.src = URL.createObjectURL(event.target.files[0]);
-  };
-  
+function information() {
+    var fullName = document.getElementById('FullName').value
+    var email = document.getElementById('email').value
+    var phoneNumber = document.getElementById('PhoneNumber').value
+    var address = document.getElementById('Adress').value
+    var codePostal = document.getElementById('CodePostal').value
+    var dateOfBirth = document.getElementById('DateOfBirth').value
+    var education = document.getElementById('Education').value
+    var otherEducation = document.getElementById('OtherEducation').value
+    var workExperience = document.getElementById('WorkExperience').value
+    var languages = document.getElementById('languages').value
+    var skills = document.getElementById('Skills').value
+
+
+    var cv = {
+        fullName: fullName,
+        email: email,
+        phoneNumber: phoneNumber,
+        address: address,
+        codePostal: codePostal,
+        dateOfBirth: dateOfBirth,
+        education: education,
+        otherEducation: otherEducation,
+        workExperience: workExperience,
+        languages: languages,
+        skills: skills
+    }
+
+    var cvJSON = JSON.stringify(cv)
+    localStorage.setItem('cv', cvJSON)
+    window.location.href = 'pair3.html'
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('generateCVBtn').addEventListener('click', information)
+})
+function displayCV() {
+    var cvJSON = localStorage.getItem('cv')
+    if (cvJSON) {
+        var cv = JSON.parse(cvJSON)
+
+        document.getElementById('fullName').innerText = cv.fullName
+        document.getElementById('email').innerText = cv.email
+        document.getElementById('phoneNumber').innerText = cv.phoneNumber
+        document.getElementById('address').innerText = cv.address
+        document.getElementById('codePostal').innerText = cv.codePostal
+        document.getElementById('dateOfBirth').innerText = cv.dateOfBirth
+        document.getElementById('education').innerText = cv.education
+        document.getElementById('otherEducation').innerText = cv.otherEducation
+        document.getElementById('workExperience').innerText = cv.workExperience
+        document.getElementById('languages').innerText = cv.languages
+        document.getElementById('skills').innerText = cv.skills
+
+    }
+}
+
+window.addEventListener('load', displayCV)
